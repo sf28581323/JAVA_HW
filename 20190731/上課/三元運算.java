54de5c0410d5d816fb6f40ccc150542b4cc25c34
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-07-31 14:59:42 +0800

    Create 三元運算.java

---
+++ 20190731/上課/三元運算.java
@@ -0,0 +1,18 @@
+    
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		int input;
+		input = s.nextInt();
+		
+		int answer = (input != 0) ? 1 : 0;
+		//三元運算
+		
+		System.out.print(answer);
+		
+	}
+
+}
