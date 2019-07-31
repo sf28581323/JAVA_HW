202eff39e8e30832aec92dc8e3db4a6ba7681e42
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-07-31 14:13:32 +0800

    Create 長方體體積.java

---
+++ 20190731/上課/長方體體積.java
@@ -0,0 +1,15 @@
+    
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		double w = scanner.nextDouble();
+		double h = scanner.nextDouble();
+		double l = scanner.nextDouble();
+		double v = w * h * l ;
+		System.out.print(v);
+	}
+
+}
