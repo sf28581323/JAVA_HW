6646f7d00ac7473b56579a97cef8b741575e48e2
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-02 13:56:42 +0800

    Create 不能有偶數.java

---
+++ 20190731/HW/不能有偶數.java
@@ -0,0 +1,17 @@
+    
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+	java.util.Scanner s = new java.util.Scanner(System.in);
+	int a = s.nextInt();
+	while(a%2==0) {
+		a = a/2;
+	}
+	
+	System.out.print(a);
+			
+	}
+
+}
