f89314b90d301d0502d3875a4318a9b57f261e9e
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-02 15:14:18 +0800

    Create 副程式三整數相加.java

---
+++ 20190802/上課/副程式三整數相加.java
@@ -0,0 +1,17 @@
+    
+package test;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		int sum = add(100,200,300);
+		System.out.print(sum);
+	}
+	
+	public static int add(int n1,int n2,int n3) {
+		return n1+n2+n3;
+	}
+	
+}
