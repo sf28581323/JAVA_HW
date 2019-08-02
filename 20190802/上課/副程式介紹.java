2c20d29bed8ba86ce50058871b4b4ddeaf2b4de5
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-02 15:09:00 +0800

    Create 副程式介紹.java

---
+++ 20190802/上課/副程式介紹.java
@@ -0,0 +1,18 @@
+    
+package test;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		int sum = my(100, 200);
+		
+		System.out.print(sum);
+	}
+	
+	public static int my(int num1, int num2) {
+		return num1 + num2;
+	}
+
+}
