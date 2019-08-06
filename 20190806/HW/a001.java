c807834cf5d9796a3ef15d3bf27e0d9718dfd8e2
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-06 14:41:50 +0800

    Create a001.java

---
+++ 20190806/HW/a001.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String s;
+		while(scanner.hasNext()) {
+			s = scanner.nextLine();
+			System.out.println("hello, "+s);
+		}
+			
+	}
+}
+
