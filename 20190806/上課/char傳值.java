129b8d4ecb302ea03ff9cc68d94f363abe1728b5
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-06 13:51:23 +0800

    Create char傳值.java

---
+++ 20190806/上課/char傳值.java
@@ -0,0 +1,23 @@
+package test;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		char x = '1';
+		
+		System.out.print(x);
+		
+		
+		my(x);
+		System.out.print(x);
+		
+		
+	}
+	
+	public static void my(char x) {
+		x = '2';
+		System.out.print(x);
+		
+	}
+}
