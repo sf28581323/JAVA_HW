6adbd246fc6da3f5789f9b4d941e1a0f75ef449f
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-08-07 14:35:27 +0800

    Create 陣列長度.java

---
+++ 2019-08-07/上課/陣列長度.java
@@ -0,0 +1,16 @@
+package test;
+	
+import java.util.Scanner;
+	
+public class test {
+	public static void main(String[] args) {
+		int[][] array = {
+				{1,2,3,4},
+				{5,6,7,8}
+				};
+		
+		System.out.print("陣列長度:" + array.length * array[0].length);
+		
+	}
+}
+	
