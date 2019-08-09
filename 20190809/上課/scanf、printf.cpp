4ee755346b63b7ea46d8f6df4a91857f3976304c
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-08-09 12:29:29 +0800

    Create scanf、printf.cpp

---
+++ 20190809/上課/scanf、printf.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	int a,b,c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d", a);
+	
+	return 0;
+}
