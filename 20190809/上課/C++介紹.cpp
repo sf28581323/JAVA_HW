5a223e9c0bdeea508ebb31c2a1c6a7fd5855272e
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-08-09 11:56:50 +0800

    Create C++介紹.cpp

---
+++ 20190809/上課/C++介紹.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	int a = 100;
+	printf("%d", a);
+	cin >> a;
+	cout << a<< endl<< endl<< endl<< endl<< endl<< endl;
+	
+	cout << "String" << endl;
+	cout << "String" << endl;
+	cout << "This is A: " << a << endl;
+	
+	
+	return 0;
+}
