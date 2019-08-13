348c29b3e17c7d01507d82365c70c81e4272457b
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-13 16:18:53 +0000

    Create a002.java

---
+++ 20190806/HW/a002.java
@@ -0,0 +1,19 @@
+package text;
+
+
+public class text {
+
+	private static java.util.Scanner scanner;
+
+	public static void main(String[] args) {
+		scanner = new java.util.Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a+b);
+
+
+	}
+	
+
+
+}
