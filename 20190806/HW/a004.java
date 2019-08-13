de9744db271112359a1ab52d52e674e0001cce98
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-13 16:40:07 +0000

    Create a004.java

---
+++ 20190806/HW/a004.java
@@ -0,0 +1,27 @@
+package text;
+
+
+public class text {
+
+	private static java.util.Scanner scanner;
+
+	public static void main(String[] args) {
+		scanner = new java.util.Scanner(System.in);
+		int a = scanner.nextInt();
+		if((a%4==0 && a%100!=0)||a%400==0) {
+			System.out.println("閏年");
+		}
+		else {
+			System.out.println("平年");
+		}
+
+		
+		
+		
+
+
+	}
+	
+
+
+}
