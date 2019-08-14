7bfc54acd2c1ff6fc38e6e64466c0f8248024f62
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-08-14 14:08:41 +0800

    Update a004.java

--- 20190806/HW/a004.java
+++ 20190806/HW/a004.java
@@ -7,21 +7,16 @@ public class text {
 
 	public static void main(String[] args) {
 		scanner = new java.util.Scanner(System.in);
-		int a = scanner.nextInt();
-		if((a%4==0 && a%100!=0)||a%400==0) {
-			System.out.println("閏年");
+		
+		while(scanner.hasNext()) {
+			int a = scanner.nextInt();
+			if((a%4==0 && a%100!=0)||a%400==0) {
+				System.out.println("閏年");
+			}
+			else {
+				System.out.println("平年");
+			}
 		}
-		else {
-			System.out.println("平年");
-		}
-
 		
-		
-		
-
-
 	}
-	
-
-
 }
