4ec7f42bcece4b2c638f79529c8b5a4fddaecb50
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-19 18:16:53 +0000

    Create a006.java

---
+++ 20190814/HW/a006.java
@@ -0,0 +1,28 @@
+package text;
+
+
+public class text {
+
+	private static java.util.Scanner scanner;
+
+	public static void main(String[] args) {
+		scanner = new java.util.Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		
+		while(scanner.hasNext()) {
+			int temp = b*b-4*a*c;
+			if(temp>0) {
+				System.out.println("Two different roots x1="+(-b+temp)/(2*a)+" , "+"x2="+(-b-temp)/(2*a));
+			}
+			else if(temp==0) {
+				System.out.println("Two same roots x="+-b/(2*a));
+			}
+			else if(temp<0) {
+				System.out.println("No real root");
+			}
+		}
+		
+	}
+}
