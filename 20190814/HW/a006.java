f0ff90779978d3b992b0c58cc6417e6bae92d6de
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-08-20 13:47:54 +0800

    Update a006.java

--- 20190814/HW/a006.java
+++ 20190814/HW/a006.java
@@ -1,17 +1,20 @@
-package text;
+    
+package test;
 
 
-public class text {
+public class test {
 
 	private static java.util.Scanner scanner;
 
 	public static void main(String[] args) {
 		scanner = new java.util.Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		int c = scanner.nextInt();
-		
 		while(scanner.hasNext()) {
+			
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			int c = scanner.nextInt();
+			
+			
 			int temp = b*b-4*a*c;
 			if(temp>0) {
 				System.out.println("Two different roots x1="+(-b+temp)/(2*a)+" , "+"x2="+(-b-temp)/(2*a));
