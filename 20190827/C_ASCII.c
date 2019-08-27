9789c485576bf157c9c6b41d243772ec29ead45d
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-08-27 15:02:13 +0800

    Create C_ASCII.c

---
+++ 20190827/C_ASCII.c
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	char ch='0';
+	printf("%c\n", ch);
+	printf("%d\n", ch);
+	
+	int a = 7;
+	printf("%c\n", a);
+	printf("%d\n", a);
+	
+	for(int i = 0; i < 100; i++)
+		printf("%c", i);
+
+	// cin >> ch;
+	// scanf("%c", &ch);
+	// ch = getchar();
+	// ch = getch();
+	// ch = getche();
+	
+	return 0;
+}
