68743eaa558689d544770cf00b200c92d11941e0
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-28 15:00:44 +0800

    Create sizeof.cpp

---
+++ 20190828/sizeof.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	long int b;
+	long long int c;
+	
+	cout << sizeof(a) << " " << sizeof(b) << " " << sizeof(c) << " ";
+	
+	// long int
+	// long
+	// int 
+	// ===↑ 一樣的東西
+	
+	
+	// ===↓ 一樣的東西 
+	// long long 
+	// long long int  
+	
+	return 0;
+}
