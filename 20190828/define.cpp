2d35c5cdf5601654405ee68b3b62f38e031026e1
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-28 15:12:41 +0800

    Create define.cpp

---
+++ 20190828/define.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define PI 3.14
+#define multi(x) x*xa
+#define add(a,b) a+b
+
+using namespace std;
+
+int main() {
+	
+	cout << PI << endl;
+	cout << multi(10) << endl;
+	cout << add(10,30) * 2 << endl;
+	
+	return 0;
+}
+
