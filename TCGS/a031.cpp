fcfd3062409748cc8adeaa59e7e140f76f3ef35e
sf28581323 <53506745+sf28581323@users.noreply.github.com>
2019-08-28 14:58:14 +0800

    Create a031.cpp

---
+++ TCGS/a031.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int A;
+	long long int B;
+	cin >> A;
+	cin >> B;
+	
+	if(A>B){
+		cout << "A" << " " << A+B << endl;
+	}
+	else{
+		cout << "B" << " " << A+B << endl;
+	}
+	
+	
+	return 0;
+}
